dba55eed029e4db214ba1353cc5a00535d30fb38
sajid shaikh <sajid20shaikh@gmail.com>
2022-03-07 19:10:05 +0530

    one more added

---
+++ gettingstarted/factorial.py
@@ -0,0 +1,17 @@
+def primeFac(n):
+    # write your code here
+    div=2
+    while n!=1:
+        if n%div==0:
+            n=n//div
+            print(div,end=" ")
+        if n%div!=0:
+            div+=1
+         
+
+def main():
+    n = int(input())
+    primeFac(n)
+
+if __name__=="__main__":
+    main()
