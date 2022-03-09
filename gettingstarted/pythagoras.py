2321510dd1319e2509fb5c609df1fdb4ed554e02
sajid shaikh <sajid20shaikh@gmail.com>
2022-03-09 23:10:04 +0530

    added code in pyhthagoras

--- gettingstarted/pythagoras.py
+++ gettingstarted/pythagoras.py
@@ -2,8 +2,13 @@ def main():
     # input
     arr = input().split(" ")
     a = int(arr[0])
-    b= int(arr[1])
-    c=int(arr[2])
+
+    try:
+        b = int(arr[1])
+        c = int(arr[2])
+    except:
+        b = int(input())
+        c = int(input())
     if a<b:
         temp=a
         a=b
@@ -12,18 +17,10 @@ def main():
         temp=a
         a=c
         c=temp
-    if a*a==b*b+c*c:
+    if (a*a)==(b*b+c*c):
         print("true")
     else:
         print("false")
-    try:
-        b = int(arr[1])
-        c = int(arr[2])
-    except:
-        b = int(input())
-        c = int(input())
-    
 
 if __name__ == "__main__":
     main()
-    
