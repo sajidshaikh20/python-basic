2252303dc2270abb5b2fa34bdf5a62dcbb777b41
sajid shaikh <sajid20shaikh@gmail.com>
2022-03-11 00:23:55 +0530

    maximum number

--- gettingstarted/max2nd.py
+++ gettingstarted/max2nd.py
@@ -1,10 +1,12 @@
-arr=[2,5,7,1,42,31]
+arr=[2,5,7,545641,4222,3114]
 max1=arr[0]
 max2=arr[1]
-for value in arr :
-    if value>max1:
-        max1=value 
-    else:
-        max2=value
-
+n =len(arr)
+for i in range(2,n):
+    if arr[i]>max1:
+        max2=max1
+        max1=arr[i]
+    elif arr[i]>max2:
+        max1!=arr[i]
+        max2=arr[i]
 print(max2)
