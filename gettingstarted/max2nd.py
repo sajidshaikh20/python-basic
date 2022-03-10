4f4b9ab160b0ffd92d8ed92738660b6f86476f15
sajid shaikh <sajid20shaikh@gmail.com>
2022-03-10 23:33:31 +0530

    completed

--- gettingstarted/max2nd.py
+++ gettingstarted/max2nd.py
@@ -1,9 +1,10 @@
-arr=[2,5,7,-1,42,31]
+arr=[2,5,7,1,42,31]
 max1=arr[0]
 max2=arr[1]
 for value in arr :
-    if max1<value:
-        temp=max1
-        max1=value
-        max2=temp
+    if value>max1:
+        max1=value 
+    else:
+        max2=value
+
 print(max2)
